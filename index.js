40ce6f303830e89046c60365efe85adcf1d03d57
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-02-14 22:03:43 -0600

    added more code to function at the bottom of the index file

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 // packages needed for this application
-const fs = require('fs');
+const {writeFile} = require('./utils/generateMarkdown');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generatePage = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
-const questions = () => {
+const promptQuestions = () => {
     return inquirer.prompt([
         {
             type: 'input',
             name: 'title',
             messages: 'What is your project title? (Required)'
         },
+        {
+            type: 'input',
+            name: 'description',
+            messages: 'What is the description of this project?'
+        }
         
-    ]);
+    ]).then(projectData => {
+        READMEdata.projects.push(READMEdata);
+        if (projectData.confirmAddData) {
+            return promptQuestions(READMEdata);
+        }else {
+            return READMEdata;
+        }
+    });
   
 
     };
 
 
 // TODO: Create a function to write README file
- fs.writeToFile('index.html', generateMarkdown(), err => {
+ fs.writeToFile('index.html', promptQuestions(), err => {
      if (err) throw err;
      console.log('REAME complete! Check the html file!');
  });
